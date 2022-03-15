00a68784b2d5f8722d0580c25065333554f08692
salkachow <101236253+salkachow@users.noreply.github.com>
2022-03-16 00:03:38 +0700

    Create 1.student.mark.py

---
+++ 1.student.mark.py
@@ -0,0 +1,56 @@
+student = {"id":None,"name":None,"DoB":None}
+course = {"id":None,"name":None}
+stu_list = []
+crs_list = []
+def add_stu():
+      while(True):
+        n = int(input("enter number of students: "))
+        if(n>0): break
+    for i in range(n):
+        print("student "+str(i+1))
+        student["id"] = input("enter id: ")
+        student["name"] = input("enter name: ")
+        student["DoB"] = input("enter date of birth: ")
+        stu_list.append(student.copy())
+def list_stu():
+       print("here is student list:")
+    print("STT\t id\t name\t DoB")
+    for i in range(len(stu_list)):
+        s = stu_list[i]
+        print((i+1),"\t",s["id"],"\t",s["name"],"\t",s["DoB"])
+def add_crs():
+       while(True):
+        n = int(input("enter number of courses: "))
+        if(n>0): break
+    for i in range(n):
+        print("course "+str(i+1))
+        course["id"] = input("enter id: ")
+        course["name"] = input("enter name: ")
+        crs_list.append(course.copy())
+def list_crs():
+      print("here is course list:")
+    print("STT\t id\t name")
+    for i in range(len(crs_list)):
+        c = crs_list[i]
+        print((i+1),"\t",c["id"],"\t",c["name"])
+def add_mark():
+      a = input("choose a course: ")
+    for i in range(len(stu_list)):
+        print("student "+str(i+1))
+        mark = input("enter mark: ")
+        stu_list[i].update({"mark":mark})
+    print("-----")
+    print("students' mark in",a,"course:")
+    print("STT\t id\t name\t mark")
+    for i in range(len(stu_list)):
+        s = stu_list[i]
+        print((i+1),"\t",s["id"],"\t",s["name"],"\t",s["mark"])
+add_stu()
+print("-----")
+add_crs()
+print("-----")
+list_stu()
+print("-----")
+list_crs()
+print("-----")
+add_mark()
